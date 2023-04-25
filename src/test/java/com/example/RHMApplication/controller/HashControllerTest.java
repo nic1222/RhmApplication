76cfbf6dd290ebe372aa33ec372a25b532048dea
Nicholas <nicholas9755@gmail.com>
2023-04-25 17:00:22 +0800

    test

---
+++ src/test/java/com/example/RHMApplication/controller/HashControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.RHMApplication.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashControllerTest {
+
+    @Test
+    void getHash() {
+
+    }
+}
